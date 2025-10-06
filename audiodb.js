ec1069428315ae4223b4368d6fb1774b7bf42b8f
乂ᴇᴏꋊ <125651298+xeon2account@users.noreply.github.com>
2025-10-06 19:19:43 +0530

    Create audiodb.js

---
+++ audiodb.js
@@ -0,0 +1,27 @@
+export const playlist = [
+  {
+    name: "Mr.Kitty - After Dark",
+    src: "https://files.catbox.moe/55z0xp.mp3",
+    thumb: "https://files.catbox.moe/71l6k7.jpg"
+  },
+  {
+    name: "Ranam Title Track | Prithviraj Sukumaran",
+    src: "https://files.catbox.moe/t4rztg.mp3",
+    thumb: "https://files.catbox.moe/wfyzab.jpg"
+  },
+  {
+    name: "I Wanna Be Yours",
+    src: "https://files.catbox.moe/59ursd.mp3",
+    thumb: "https://files.catbox.moe/4tr6ip.jpg"
+  },
+  {
+    name: "Avicii - The Nights",
+    src: "https://files.catbox.moe/by3cbr.mp3",
+    thumb: "https://files.catbox.moe/1dmv8w.jpg"
+  },
+  {
+    name: "Madonna - Like A Prayer",
+    src: "https://files.catbox.moe/x19j3y.mp3",
+    thumb: "https://files.catbox.moe/9g9hks.jpg"
+  }
+];
