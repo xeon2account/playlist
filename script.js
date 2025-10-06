d000963761b3d64e711b840ffffb0d9d50a6797e
乂ᴇᴏꋊ <125651298+xeon2account@users.noreply.github.com>
2025-10-06 19:08:04 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,88 @@
+// audiodb.js
+export const playlist = [
+  {
+      name: "Mr.Kitty - After Dark",
+      src: "https://files.catbox.moe/55z0xp.mp3",
+      thumb: "https://files.catbox.moe/71l6k7.jpg"
+  },
+  {
+      name: "Ranam Title Track | Lyric Video | Prithviraj Sukumaran",
+      src: "https://files.catbox.moe/t4rztg.mp3",
+      thumb: "https://files.catbox.moe/wfyzab.jpg"
+  },
+  {
+      name: "I Wanna Be Yours",
+      src: "https://files.catbox.moe/59ursd.mp3",
+      thumb: "https://files.catbox.moe/4tr6ip.jpg"
+  },
+  {
+      name: "Avicii - The Nights",
+      src: "https://files.catbox.moe/by3cbr.mp3",
+      thumb: "https://files.catbox.moe/1dmv8w.jpg"
+  },
+  {
+      name: "Madonna - Like A Prayer",
+      src: "https://files.catbox.moe/x19j3y.mp3",
+      thumb: "https://files.catbox.moe/9g9hks.jpg"
+  },
+  {
+      name: "Lady Gaga, Bruno Mars - Die With A Smile (Official Music Video)",
+      src: "https://files.catbox.moe/8ejjkm.m4a",
+      thumb: "https://files.catbox.moe/3y3yny.jpg"
+  },
+  {
+      name: "Alan Walker & Ava Max - Alone, Pt. II",
+      src: "https://files.catbox.moe/70gs6c.mp3",
+      thumb: "https://files.catbox.moe/du5ajv.jpg"
+  },
+  {
+      name: "Coldplay - Hymn For The Weekend (Official Video)",
+      src: "https://files.catbox.moe/4mppob.mp3",
+      thumb: "https://files.catbox.moe/mbbo9s.jpg"
+  },
+  {
+      name: "Lana Del Rey - Summertime Sadness (Official Music Video)",
+      src: "https://files.catbox.moe/mdvomq.mp3",
+      thumb: "https://files.catbox.moe/zqeru9.jpg"
+  },
+  {
+      name: "The Weeknd - Blinding Lights (Official Video)",
+      src: "https://files.catbox.moe/6o4jvf.mp3",
+      thumb: "https://files.catbox.moe/fr99qo.jpg"
+  },
+  {
+      name: "Ghost - Mary On A Cross (Official Music Video)",
+      src: "https://files.catbox.moe/4pgicv.mp3",
+      thumb: "https://files.catbox.moe/aoua4f.jpg"
+  },
+  {
+      name: "IN THE END (Slowed Down) - Tommee Profitt, Fleurie [Mellen Gi Remix]",
+      src: "https://files.catbox.moe/zpas8g.mp3",
+      thumb: "https://files.catbox.moe/nyywky.jpg"
+  },
+  {
+      name: " VXLLAIN, VØJ, Narvent - Distant Echoes (4K Official Music Video)",
+      src: "https://files.catbox.moe/gmq4jd.mp3",
+      thumb: "https://files.catbox.moe/uw25ar.jpg"
+  },
+  {
+      name: "VØJ, Narvent - Memory Reboot (4K Music Video)",
+      src: "https://files.catbox.moe/09w2su.mp3",
+      thumb: "https://files.catbox.moe/bm2fq0.jpg"
+  },
+  {
+      name: "Post Malone, Swae Lee - Sunflower",
+      src: "https://files.catbox.moe/512la6.mp3",
+      thumb: "https://files.catbox.moe/8xfbq1.jpg"
+  },
+  {
+      name: "Narvent - Fainted",
+      src: "https://files.catbox.moe/gme3ud.mp3",
+      thumb: "https://files.catbox.moe/weul01.jpg"
+  },
+  {
+      name: "K-391 & Alan Walker - Ignite (feat. Julie Bergan & Seungri)",
+      src: "https://files.catbox.moe/qsons4.mp3",
+      thumb: "https://files.catbox.moe/k23ytz.jpg"
+  }
+];
